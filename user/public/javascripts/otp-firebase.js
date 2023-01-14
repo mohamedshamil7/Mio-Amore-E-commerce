62cbdf050e5af919983994e2ee2648982e20907f
mohamedshamil7 <mohamedshamil0507@gmail.com>
2023-01-14 13:58:20 +0530

    git ignore update

--- user/public/javascripts/otp-firebase.js
+++
@@ -1,3 +0,0 @@
-const { initializeApp } =require ("firebase/app");
-const  { getAnalytics } =require ("firebase/analytics");
-const  { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword ,RecaptchaVerifier,signInWithPhoneNumber} = require ("firebase/auth");
